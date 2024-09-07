6e481d0a9b4ec5f3be7e22c9401b774635b869d1
Hojeong Kim <queen00220@naver.com>
2024-09-07 20:33:20 +0900

    UserRepositoryTest에 ãOptional테스í트코드추가

--- src/test/java/org/example/springv3/User/UserRepositoryTest.java
+++ src/test/java/org/example/springv3/User/UserRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.Optional;
+
 @DataJpaTest
 @Import({UserQueryRepository.class})
 public class UserRepositoryTest {
@@ -16,5 +18,27 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+
+    @Test
+    public void go(){
+        User user = null;
+        Optional<User> op = Optional.ofNullable(user);
+
+//        User userPS = op.get();
+//        System.out.println(userPS.getId()); // 1
+
+//        if(op.isPresent()){
+//            User userPS = op.get();
+//        }else{
+//            throw new RuntimeException("존재하지 않아요");
+//        } // 2
+
+//        User u = User.builder().id(1).username("ssar").build();
+//        User u2 = op.orElse(u);
+//        System.out.println(u2.getUsername()); // 3
+
+        User u = op.orElseThrow(() -> new RuntimeException("fdsafdsafdsa")); // 4
+
+    }
 
 }
